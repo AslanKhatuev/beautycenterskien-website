cbf92ad1505356c9a3ee8871491422bacf519500
Aslan Khatuev <aslan.khatuev@outlook.com>
2025-08-07 01:31:26 +0200

    endret title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export const metadata = {
-  title: "Massasje Salong",
+  title: "Beauty Center",
   description: "Velkommen til din lokale massasjesalong",
 };
 
