7643301ec439b6f9050e9f7e44f2f28188fe4397
Aslan Khatuev <aslan.khatuev@outlook.com>
2025-08-15 14:20:41 +0200

    ....

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
-  }
-}
+    turbopack: true,
+  },
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
